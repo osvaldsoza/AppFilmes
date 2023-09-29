84169be2fb0562d3c5931a5223e9d721ef16164c
Osvaldo Souza Ferreira <osvald.soza@gmail.com>
2023-09-29 12:33:05 -0300

    first commit

---
+++ app/src/main/java/br/com/osvaldsoza/appfilmes/view/FormLogin.kt
@@ -0,0 +1,12 @@
+package br.com.osvaldsoza.appfilmes.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.osvaldsoza.appfilmes.R
+
+class FormLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_login)
+    }
+}
